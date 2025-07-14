7058d12349fe06b027de6c72a251b50c714a882e
Trung Nghia <nh0cbum1234@gmail.com>
2025-07-14 13:22:21 +0700

    first commit

---
+++ detail.js
@@ -0,0 +1,104 @@
+const params = new URLSearchParams(window.location.search);
+const productId = params.get("id");
+console.log("Product ID:", params.get("id"));
+
+const productDetail = document.querySelector(".product-detail");
+const container = document.querySelector(".container");
+if (productId) {
+    fetch(`https://dummyjson.com/products/${productId}`)
+        .then((res) => res.json())
+        .then((product) => {
+            const discountedPrice = (
+                product.price *
+                (1 - product.discountPercentage / 100)
+            ).toFixed(2);
+            productDetail.innerHTML = "";
+            // Nút quay về trang danh sách sản phẩm
+            const backBtn = document.createElement("a");
+            backBtn.href = "index.html";
+            backBtn.className = "back-button";
+            backBtn.textContent = "← Quay lại danh sách sản phẩm";
+            container.appendChild(backBtn);
+
+            const imageSection = document.createElement("div");
+            imageSection.className = "product-image";
+
+            const mainImage = document.createElement("img");
+            mainImage.src = product.images[0];
+            mainImage.alt = product.title;
+            mainImage.className = "main-image";
+            mainImage.id = "mainImage";
+
+            imageSection.appendChild(mainImage);
+
+            const gallery = document.createElement("div");
+            gallery.className = "image-gallery";
+
+            product.images.forEach((img) => {
+                const thumb = document.createElement("img");
+                thumb.src = img;
+                thumb.alt = "Ảnh sản phẩm";
+                thumb.addEventListener("click", () => {
+                    mainImage.src = img;
+                });
+                gallery.appendChild(thumb);
+            });
+
+            imageSection.appendChild(gallery);
+
+            const infoSection = document.createElement("div");
+            infoSection.className = "product-info";
+
+            const title = document.createElement("h1");
+            title.className = "product-title";
+            title.textContent = product.title;
+
+            const priceDiv = document.createElement("div");
+            priceDiv.className = "product-price";
+
+            const originalPrice = document.createElement("span");
+            originalPrice.className = "original-price";
+            originalPrice.textContent = `$${product.price}`;
+
+            const discount = document.createElement("span");
+            discount.className = "discount";
+            discount.textContent = `$${discountedPrice}`;
+
+            priceDiv.appendChild(originalPrice);
+            priceDiv.appendChild(discount);
+
+            const desc = document.createElement("p");
+            desc.className = "product-description";
+            desc.textContent = product.description;
+
+            const meta = document.createElement("div");
+            meta.className = "product-meta";
+
+            meta.innerHTML = `
+        <p><span>Thương hiệu:</span> ${
+            product.brand ? product.brand : "No brand"
+        }</p>
+        <p><span>Danh mục:</span> ${product.category}</p>
+        <p><span>Đánh giá:</span> <span class="stars">★</span> ${
+            product.rating
+        }</p>
+        <p><span>Tồn kho:</span> ${
+            product.stock > 0 ? `${product.stock} sản phẩm` : "Hết hàng"
+        }</p>
+      `;
+            infoSection.appendChild(title);
+            infoSection.appendChild(priceDiv);
+            infoSection.appendChild(desc);
+            infoSection.appendChild(meta);
+
+            productDetail.appendChild(imageSection);
+            productDetail.appendChild(infoSection);
+            container.appendChild(productDetail);
+        })
+        .catch((err) => {
+            productDetail.innerHTML = `<p style="color:red;">Không tìm thấy sản phẩm với ID = ${productId}</p>`;
+        });
+} else {
+    productDetail.innerHTML =
+        '<p style="color:red;">Thiếu tham số ?id= trong URL</p>';
+}
---
+++ main.js
@@ -0,0 +1,36 @@
+const products = document.querySelector(".product-list");
+fetch("https://dummyjson.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+        data.products.forEach((product) => {
+            const productCard = document.createElement("div");
+            productCard.classList.add("product-card");
+            productCard.dataset.id = product.id;
+            const productImage = document.createElement("img");
+            productImage.src = product.thumbnail;
+            productImage.alt = product.title;
+            productCard.appendChild(productImage);
+            const productContent = document.createElement("div");
+            productContent.classList.add("product-content");
+            const productTitle = document.createElement("h3");
+            productTitle.textContent = product.title;
+            productContent.appendChild(productTitle);
+            const productDescription = document.createElement("p");
+            productDescription.textContent = product.description;
+            productContent.appendChild(productDescription);
+            productCard.appendChild(productContent);
+            const productPrice = document.createElement("span");
+            productPrice.classList.add("price");
+            productPrice.textContent = `$${product.price}`;
+            productCard.appendChild(productPrice);
+
+            //chuyển hướng đến trang chi tiết sản phẩm khi nhấp vào thẻ sản phẩm.
+            productCard.addEventListener("click", () => {
+                window.location.href = `./detail.html?id=${product.id}`;
+            });
+            products.appendChild(productCard);
+        });
+    })
+    .catch((error) => {
+        console.error("Lỗi khi tải dữ liệu:", error);
+    });
